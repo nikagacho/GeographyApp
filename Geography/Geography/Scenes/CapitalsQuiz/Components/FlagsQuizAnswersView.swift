dec9dde42c85420012db242c0f85e908d399748b
nikagacho <nika.gachechiladze@hotmail.com>
2024-01-25 21:44:19 +0400

    navigation done
    FlowNavigator class complete, which navigates from uikit to swiftui

--- Geography/Geography/Scenes/CapitalsQuiz/Components/FlagsQuizAnswersView.swift
+++ Geography/Geography/Scenes/CapitalsQuiz/Components/FlagsQuizAnswersView.swift
@@ -13,18 +13,23 @@ struct FlagsQuizAnswersView: View {
     @Binding var selectedAnswer: String?
     var correctAnswer: String
     
+    var layout: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
     var body: some View {
-        ForEach(possibleAnswers, id: \.self) { flag in
-            AsyncImage(url: URL(string: flag)) { image in
-                image.resizable()
-                    .frame(width: 100, height: 80)
-                    .border(.black, width: 5)
-            } placeholder: {
-                ProgressView()
-                    .frame(width: 100, height: 80)
-            }
-            .onTapGesture {
-                answerSelected(flag)
+        LazyVGrid(columns: layout) {
+            ForEach(possibleAnswers, id: \.self) { flag in
+                AsyncImage(url: URL(string: flag)) { image in
+                    image.resizable()
+                        .frame(height: 120)
+                        .border(.black)
+                        .cornerRadius(8)
+                } placeholder: {
+                    ProgressView()
+                        .frame(height: 100)
+                }
+                .onTapGesture {
+                    answerSelected(flag)
+                }
             }
         }
     }
